4b59b975c3bbb94ec4cfbecac92799d9adfbc638
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-04-08 09:58:15 -0400

    completed fundamentals section

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,18 +1,33 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.*;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        Set<String> palindromes = new HashSet<>();
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i + 1; j <= string.length(); j++) {
+                if (isPalindrome(string.substring(i, j))) {
+                    palindromes.add(string.substring(i, j));
+                }
+            }
+        }
+
+        String[] arr = palindromes.toArray(new String[palindromes.size()]);
+        return arr;
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+        if(string.equals(reverseString(string))) {
+            return true;
+        }
+        return false;
     }
 
     public static String reverseString(String string) {
-        return null;
+        return new StringBuilder(string).reverse().toString();
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/StringEvaluator.java
@@ -1,18 +1,50 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author leon on 11/12/2018.
  */
 public class StringEvaluator {
     public static String[] getAllSubstrings(String string) {
-        return null;
+        Set<String> noDupes = new HashSet<>();
+
+        for(int i = 0; i < string.length(); i++) {
+            for(int j = i + 1; j <= string.length(); j++) {
+                noDupes.add(string.substring(i, j));
+            }
+        }
+        String[] arr = noDupes.toArray(new String[noDupes.size()]);
+        return arr;
     }
 
     public static String[] getCommonSubstrings(String string1, String string2) {
-        return null;
+        Set<String> commonElements = new HashSet<>();
+        String[] arr1 = getAllSubstrings(string1);
+        String [] arr2 = getAllSubstrings(string2);
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i].equals(arr2[j])) {
+                    commonElements.add(arr1[i]);
+                }
+            }
+        }
+        String[] arr = commonElements.toArray(new String[commonElements.size()]);
+        return arr;
     }
 
     public static String getLargestCommonSubstring(String string1, String string2) {
-        return null;
+        String[] arr = getCommonSubstrings(string1, string2);
+        System.out.println(Arrays.toString(arr));
+        String largest = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (largest.length() < arr[i].length()) {
+                largest = arr[i];
+            }
+        }
+        return  largest;
     }
 }
